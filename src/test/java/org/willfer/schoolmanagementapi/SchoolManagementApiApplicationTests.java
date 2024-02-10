a178adea2068faa89cc408c895b451274ed71b3f
Williams E. Fernández <williamsenrique@gmail.com>
2024-02-10 14:13:44 -0600

    Commit inicial de proyecto base para Sistema de Administración de Escuela

---
+++ src/test/java/org/willfer/schoolmanagementapi/SchoolManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.willfer.schoolmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
